7846fefc0b76e8e9beaef6f53552afdd4c1328fd
Ubaldino Zurita <ozzirisc@gmail.com>
2016-12-29 21:41:26 -0400

    [ADD] dependencies checker

---
+++ dependencies_checker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cc=/usr/bin/gcc
+if [ ! -x "$cc" ]; then
+    echo "$cc not found or it's not executable"
+    exit 47
+fi
+
+cc=/usr/bin/g++
+if [ ! -x "$cc" ]; then
+    echo "$cc not found or it's not executable"
+    exit 47
+fi
+
+cc=/usr/bin/python2
+if [ ! -x "$cc" ]; then
+    echo "$cc not found or it's not executable"
+    exit 47
+fi
+
+cc=/usr/bin/javac
+if [ ! -x "$cc" ]; then
+    echo "$cc not found or it's not executable"
+    exit 47
+fi
+
+cc=/usr/bin/jar
+if [ ! -x "$cc" ]; then
+    echo "$cc not found or it's not executable"
+    exit 47
+fi
+
+echo "Dependencies Satisfied"
+
+exit 0;
